700e0bdb75cabffc1e3c2f96639efb3910538ddb
BBIO-0530 <morequackquack@gmail.com>
2026-02-22 20:49:29 +0900

    Update 05. For & While.py

--- 05. For & While.py
+++ 05. For & While.py
@@ -1,1 +1,19 @@
-
+# 반복문
+# 코드를 여러번 실행하고 싶을 때 사용하는 문법이다.
+#for문
+#for문은 주로 리스트, 문자열, 튜플 등과 같은 각 항목에 순서대로 접근해야 할때 사용합니다.
+BND=["onedoor","members","ZICO"]
+for i in BND:
+    print(i)
+#range 함수
+#for문에서 반복 횟수를 지정해줄 수 있는 함수이다.
+for a in range(10):
+    print("BND the G.O.A.T")
+#기본 사용법
+# range(n)은 0부터 n-1까지의 숫자를 생성합니다.
+# 시작점과 끝 지점
+# range(start, stop)을 사용하면 start부터 stop-1까지의 숫자를 생성합니다.
+# 증가폭 지정
+# range(start, stop, step)을 사용하여 증가폭을 지정할 수 있습니다.
+# 감소폭 지정
+# range(start, stop, step)을 사용하여 감소폭을 지정할 수 있습니다.
